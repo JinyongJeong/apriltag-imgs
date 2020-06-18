912a8bd5224fe9f7aa3b8090684084e44e5e8588
JinyongJeong <jjy0923@gmail.com>
2020-06-18 15:20:40 +0900

    Input output size as pixel

--- convert.sh
+++ convert.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 read -p 'folder name: ' folder_name
-read -p 'scale: %(only number))" ' scale
+read -p 'width: px (please input only number) ' width
+read -p 'height: px (please input only number) ' height
 
-#scale = "$scale\%"
+# initialize 
+scale=100
+# minimum size of pixel of markers
+minimumImg=5
 
-mkdir -p $scale/$folder_name
+if [ $width -gt $height ]
+then
+	scale=$((width/minimumImg))
+else 
+	scale=$((height/minimumImg))
+fi
 
-for file in ./$folder_name/*; do
-	echo $file
-	echo "$scale%"
-	convert $file -scale "$scale%" "$scale/$file"
+# convert to percent
+
+scale=$((scale*100))
+
+
+mkdir -p $width\x$height/$folder_name
+
+for file in ./$folder_name/tag*.png; do
+	convert $file -scale "$scale%" -resize $width\x$height! $width\x$height/$file
+	echo  $file
 done
 
-echo $folder_name $scale
-#convert tag.png -scale 5000% tag3.png
